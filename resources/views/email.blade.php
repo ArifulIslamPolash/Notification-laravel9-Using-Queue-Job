a2eebfead3483d1181bd2b5a6a6682a6ae997751
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-09-12 00:57:06 +0600

    new custom file added in emal body send

---
+++ resources/views/email.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello , </h1>
+    <h2>Ariful Islam Polash</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero mollitia error quas nemo odio quidem, minima quos laborum sint quasi, tempora rerum voluptatibus vel! Possimus odio earum nobis in nisi officia placeat eos, totam delectus dolores, ratione quae autem ullam laboriosam ipsa consequatur quo? Suscipit natus dignissimos eos laborum voluptas?</p>
+</body>
+</html>
